ef5072d2f87b72173d408058ec2579bf8b308296
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-05 14:32:24 -0500

    creando la clase destination en el archivo models.py

--- telusko/travello/models.py
+++ telusko/travello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+    nombreCiudad = models.CharField(max_length=100)
+    descripcionCiudad = models.TextField()
+    imagenCiudad = models.ImageField(upload_to='pics')
+    precioTour = models.IntegerField()
+    ofertaTour = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['id']
