140741a60f1580cb45e3f346e36a7ce9d66dcbf8
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-05 14:49:02 -0500

    modificando el models.py, views.py y agregando la configuracion necesaria para usar una base de datos PostgreSQL

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    
+    dests = Destination.objects.all()
+    
+    return render(request, "index.html", {'dests': dests})
+
+def destinationCreate(request):
+    form = DestinationForm(request.POST, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        form = DestinationForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'destinationCreate.html', context)
